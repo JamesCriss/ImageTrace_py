a6d4508d4828f6149003718a6f9a1ab957ae20c8
JamesCriss <james.crispro@gmail.com>
2025-02-15 17:19:21 +0100

    Working_small_islands_filtration

--- ImageTrace.py
+++ ImageTrace.py
@@ -2,14 +2,15 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageChops, ImageOps
 import numpy as np
 from scipy.ndimage import label
 
+
 # Load image
 image_original = Image.open('automobile-5128760_1280.jpg')
 
 # Apply median filter to reduce noise
-image_filtered = image_original.filter(ImageFilter.MedianFilter(size=9))
+image_filtered = image_original.filter(ImageFilter.MedianFilter(size=3))
 
 # Reduce the number of colors to 9 using adaptive palette
-num_colors = 9
+num_colors = 2
 image_reduced = image_filtered.convert("P", palette=Image.ADAPTIVE, colors=num_colors)
 
 # Convert the reduced image to an RGB mode for processing
@@ -21,30 +22,30 @@ image_array = np.array(image_reduced_rgb)
 # Get unique colors in the image
 unique_colors = np.unique(image_array.reshape(-1, image_array.shape[2]), axis=0)
 
+
 # Dictionary to store labeled islands and their corresponding colors
 island_data = np.zeros((image_array.shape[0], image_array.shape[1], 2), dtype=int)
 
-# Threshold for minimum island size
-min_pixels = 50
-
 for color_idx, color in enumerate(unique_colors, start=1):
     # Create a binary mask for the current color
+    print("coloridx:", color_idx)
     mask = np.all(image_array == color, axis=-1).astype(np.uint8)
     
     # Label connected components (floating islands)
     labeled_array, num_features = label(mask)
-    
-    # Remove small islands
-    for island_id in range(1, num_features + 1):
-        if np.sum(labeled_array == island_id) < min_pixels:
-            labeled_array[labeled_array == island_id] = 0
-    
+    island_size = np.bincount(labeled_array.flatten())
+    for i in range(1, num_features + 1):
+        if island_size[i] < 100:
+            mask[labeled_array == i] = 0
+
+    labeled_array, num_features = label(mask)
     # Store island index and color index in island_data
-    mask = labeled_array > 0  # Update mask after filtering
-    island_data[mask, 0] = labeled_array[mask]  # Island index
-    island_data[mask, 1] = color_idx  # Color index
-    
-    print(f"Color {tuple(color)} has {np.max(labeled_array)} floating islands after filtering")
+    island_data[mask == 1, 0] = labeled_array[mask == 1]  # Island index
+    island_data[mask == 1, 1] = color_idx  # Color index
+
+    island_pixels = len(island_data[:, :, 0])
+    print("Island pixels:", island_pixels)
+    print(f"Color {tuple(color)} has {num_features} floating islands")
 
 # Show images
 image_reduced.show()
